becb5a6a7f0a2c23b80ab1bfa77383edcafd1827
rhtjddls123 <jjjk0605@naver.com>
2025-04-01 22:22:52 +0900

    style: AddModal 반응형 스타일링

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ const Input = ({ ...props }: InputHTMLAttributes<HTMLInputElement>) => {
   return (
     <input
       autoFocus={false}
-      className="w-full h-10 gap-1 px-4 border body-med-14 text-gray-01 rounded-xl border-gray-05 focus:outline-0"
+      className="w-[272px] md:w-[312px] h-[41px] gap-1 px-4 border body-med-14 text-gray-01 rounded-xl border-gray-05 focus:outline-0"
       {...props}
     />
   );
--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -11,7 +11,7 @@ const Selector = <T,>({ selectTitle, selectList, ...props }: SelectorProps<T>) =
   const [data, setData] = useState<T>();
 
   let classes =
-    "flex items-center justify-between w-full h-10 gap-1 px-4 border cursor-pointer rounded-t-xl border-gray-05 focus:outline-0 ";
+    "flex items-center justify-between w-[272px] md:w-[312px] h-[41px] gap-1 px-4 border cursor-pointer rounded-t-xl border-gray-05 focus:outline-0 ";
 
   if (!open) {
     classes += "rounded-b-xl";
@@ -39,7 +39,7 @@ const Selector = <T,>({ selectTitle, selectList, ...props }: SelectorProps<T>) =
               <li key={idx}>
                 <button
                   onClick={() => handleSelect(data)}
-                  className="w-full h-10 px-4 text-left bg-white body-med-14 text-gray-01"
+                  className="w-[272px] md:w-[312px] h-[41px] px-4 text-left bg-white body-med-14 text-gray-01"
                 >
                   {String(data)}
                 </button>
