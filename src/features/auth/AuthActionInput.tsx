72a5fc161236c90dbf056347c7279a80e5d3818f
rhtjddls123 <jjjk0605@naver.com>
2025-04-01 21:55:47 +0900

    style: AuthActionInput 버튼 크기 변경

--- src/features/auth/AuthActionInput.tsx
+++ src/features/auth/AuthActionInput.tsx
@@ -12,7 +12,7 @@ const AuthActionInput = ({ buttonText, buttonProps, inputProps }: AuthActionInpu
     <div className="flex gap-2 w-[272px] md:w-[312px]">
       <AuthInput type="text" {...inputProps} />
       <button
-        className="w-20 px-4 py-3 rounded-lg shrink-0 text-primary-var1 bg-primary-var2 caption-med-12 whitespace-nowrap"
+        className="px-4 py-3 rounded-lg min-w-14 max-w-[74px] shrink-0 text-primary-var1 bg-primary-var2 caption-med-12 whitespace-nowrap"
         {...buttonProps}
       >
         {buttonText}
