b27e948d5841af444cddc6baf63e241a777615b8
rhtjddls123 <jjjk0605@naver.com>
2025-04-14 21:43:20 +0900

    refactor: Input컴포넌트가 className을 입력받을 수 있도록 변경

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,17 @@
 import { InputHTMLAttributes } from "react";
 
-const Input = ({ ...props }: InputHTMLAttributes<HTMLInputElement>) => {
-  return (
-    <input
-      autoFocus={false}
-      className="w-[272px] md:w-[312px] h-[41px] gap-1 px-4 border body-med-14 text-gray-01 rounded-xl border-gray-05 focus:outline-0"
-      {...props}
-    />
-  );
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+const Input = ({ className, ...props }: InputProps) => {
+  let classes =
+    "w-[272px] md:w-[312px] h-[41px] gap-1 px-4 border body-med-14 text-gray-01 rounded-xl border-gray-05 focus:outline-0";
+  if (className) {
+    classes += ` ${className}`;
+  }
+
+  return <input autoFocus={false} className={classes} {...props} />;
 };
 
 export default Input;
