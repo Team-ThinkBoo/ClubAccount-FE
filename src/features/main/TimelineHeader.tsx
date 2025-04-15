5e31c324b4aa63e0ffe2f255ea5deac37a9700f1
rhtjddls123 <jjjk0605@naver.com>
2025-04-15 16:20:38 +0900

    fix: 로그인 상태일때만 영수층 추가 버튼 생기도록 변경

--- src/features/main/TimelineHeader.tsx
+++ src/features/main/TimelineHeader.tsx
@@ -6,6 +6,7 @@ import BillIcon from "../../icons/BillIcon";
 import AddModal from "./AddModal";
 import { AddModalType } from "../../types/types";
 import Datepicker, { DateValueType } from "react-tailwindcss-datepicker";
+import { useAuthStore } from "../../store/useAuthStore";
 
 const TimelineHeader = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -15,6 +16,7 @@ const TimelineHeader = () => {
     startDate: null,
     endDate: null
   });
+  const { isLoggedIn } = useAuthStore();
 
   const handleOpenModal = (type: AddModalType) => {
     setModalType(type);
@@ -57,42 +59,43 @@ const TimelineHeader = () => {
               toggleIcon={() => <ArrowDownIcon />}
             />
           </div>
+          {isLoggedIn && (
+            <div className="relative">
+              <div
+                onClick={handleToggleAddMenu}
+                className="flex items-center justify-center gap-1 cursor-pointer"
+              >
+                <AddIcon />
+                <p>추가하기</p>
+              </div>
 
-          <div className="relative">
-            <div
-              onClick={handleToggleAddMenu}
-              className="flex items-center justify-center gap-1 cursor-pointer"
-            >
-              <AddIcon />
-              <p>추가하기</p>
+              {openAddMenu && (
+                <ul className="absolute right-0 flex flex-col justify-center gap-2 px-2 py-2 shadow-md rounded-xl w-max bg-warm-gray-03">
+                  <li
+                    className="flex items-center gap-2 p-2 cursor-pointer rounded-xl hover:bg-warm-gray-01 focus:bg-warm-gray-01 active:bg-warm-gray-01"
+                    onClick={() => {
+                      handleOpenModal("self");
+                      handleToggleAddMenu();
+                    }}
+                  >
+                    <ArchiveUpIcon className="w-4 h-4" />
+                    <p className="body-med-14">직접 등록하기</p>
+                  </li>
+
+                  <li
+                    onClick={() => {
+                      handleOpenModal("receipt");
+                      handleToggleAddMenu();
+                    }}
+                    className="flex items-center gap-2 p-2 cursor-pointer rounded-xl hover:bg-warm-gray-01 focus:bg-warm-gray-01 active:bg-warm-gray-01"
+                  >
+                    <BillIcon className="w-4 h-4" />
+                    <p className="body-med-14">영수증 스캔하기</p>
+                  </li>
+                </ul>
+              )}
             </div>
-
-            {openAddMenu && (
-              <ul className="absolute right-0 flex flex-col justify-center gap-2 px-2 py-2 shadow-md rounded-xl w-max bg-warm-gray-03">
-                <li
-                  className="flex items-center gap-2 p-2 cursor-pointer rounded-xl hover:bg-warm-gray-01 focus:bg-warm-gray-01 active:bg-warm-gray-01"
-                  onClick={() => {
-                    handleOpenModal("self");
-                    handleToggleAddMenu();
-                  }}
-                >
-                  <ArchiveUpIcon className="w-4 h-4" />
-                  <p className="body-med-14">직접 등록하기</p>
-                </li>
-
-                <li
-                  onClick={() => {
-                    handleOpenModal("receipt");
-                    handleToggleAddMenu();
-                  }}
-                  className="flex items-center gap-2 p-2 cursor-pointer rounded-xl hover:bg-warm-gray-01 focus:bg-warm-gray-01 active:bg-warm-gray-01"
-                >
-                  <BillIcon className="w-4 h-4" />
-                  <p className="body-med-14">영수증 스캔하기</p>
-                </li>
-              </ul>
-            )}
-          </div>
+          )}
         </div>
       </div>
     </>
