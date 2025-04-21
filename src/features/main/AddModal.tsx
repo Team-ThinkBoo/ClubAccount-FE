a5e3e6e5f972b0ec869e1afab4582dfe385bcf13
rhtjddls123 <jjjk0605@naver.com>
2025-04-21 13:05:45 +0900

    feat: invalidateQueries를 통해 영수증 등록한 후 cache초기화

--- src/features/main/AddModal.tsx
+++ src/features/main/AddModal.tsx
@@ -17,6 +17,7 @@ import {
 import { CATEGORY } from "../../constants/constants";
 import { formatDate } from "../../utils/util";
 import ReceiptDetailsList from "./ReceiptDetailsList";
+import { queryClient } from "../../utils/http";
 
 const categoryKeys = Object.keys(CATEGORY) as CategoryKeyType[];
 interface AddModalProps {
@@ -99,6 +100,7 @@ const AddModal = ({ type, open, onCloseModal }: AddModalProps) => {
     mutationFn: createReceipt,
     onSuccess: () => {
       onCloseModal();
+      queryClient.invalidateQueries({ queryKey: ["receipts"] });
     },
     onError: (err) => {
       alert(err);
