4683b51b9370e6e7f181bd7d474e74d83f7d6ef7
rhtjddls123 <jjjk0605@naver.com>
2025-04-21 13:15:20 +0900

    feat: fallback 컴포넌트 추가

--- src/features/main/TimelineContents.tsx
+++ src/features/main/TimelineContents.tsx
@@ -4,8 +4,9 @@ import TimelineContentCard from "./TimelineContentCard";
 
 const TimelineContents = () => {
   const link = localStorage.getItem("link") || "";
-  const { receipts, fetchNextPage, hasNextPage, isFetchingNextPage } = useLoadReceipts(link);
-
+  const { receipts, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useLoadReceipts(link);
+  console.log(status);
   const observerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -27,13 +28,28 @@ const TimelineContents = () => {
     return () => observer.disconnect();
   }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
+  let fallback;
+  if (status === "pending") {
+    fallback = <p className="text-center">영수증 내역을 불러오는 중입니다...</p>;
+  }
+  if (status === "error") {
+    fallback = (
+      <p className="text-center">오류가 발생하였습니다! 올바른 경로로 다시 접속해주세요!</p>
+    );
+  }
+
   return (
-    <ul className="grid grid-cols-1 min-w-[312px] w-full max-w-[568px] mx-auto gap-4 p-6 md:px-0 mid:grid-cols-2 mid:min-w-[658px] mid:max-w-[990px] lg-mid:grid-cols-3 lg-mid:w-[992px]">
-      {receipts.length === 0 && <p>지출 내역을 추가해주세요!</p>}
-      {receipts.length > 0 &&
-        receipts.map((receipt) => <TimelineContentCard key={receipt.id} receipt={receipt} />)}
-      <div ref={observerRef} style={{ height: "1px" }} />
-    </ul>
+    <>
+      <ul className="grid grid-cols-1 min-w-[312px] w-full max-w-[568px] mx-auto gap-4 p-6 md:px-0 mid:grid-cols-2 mid:min-w-[658px] mid:max-w-[990px] lg-mid:grid-cols-3 lg-mid:w-[992px]">
+        {receipts.length > 0 &&
+          receipts.map((receipt) => <TimelineContentCard key={receipt.id} receipt={receipt} />)}
+        <div ref={observerRef} style={{ height: "1px" }} />
+      </ul>
+      {fallback}
+      {status === "success" && receipts.length === 0 && (
+        <p className="text-center">지출 내역을 추가해주세요!</p>
+      )}
+    </>
   );
 };
 
