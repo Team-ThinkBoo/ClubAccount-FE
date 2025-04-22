29b57abb240c69c4f712c470625a172e837de30e
rhtjddls123 <jjjk0605@naver.com>
2025-04-22 15:04:34 +0900

    feat: LabelInput이 수정 버튼을 가질 수 있도록 변경

--- src/features/mypage/LabelInput.tsx
+++ src/features/mypage/LabelInput.tsx
@@ -5,11 +5,12 @@ interface EditModeProps extends InputHTMLAttributes<HTMLInputElement> {
   labelTitle: ReactNode;
   value: HTMLInputElement["value"];
   mode: "edit";
+  Button?: ReactNode;
 }
 
 interface ViewModeProps extends HTMLAttributes<HTMLDivElement> {
   labelTitle: ReactNode;
-  value: ReactNode;
+  value: string;
   mode: "view";
 }
 
@@ -19,12 +20,18 @@ const LabelInput = (props: LabelInputProps) => {
   const { labelTitle, mode } = props;
 
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center w-[272px] md:w-[312px]">
       <div className="flex flex-col w-full gap-1">
-        <label className="mx-2 caption-med-12 text-gray-03">{labelTitle}</label>
+        <label className="mx-2 caption-med-12 text-gray-03" htmlFor={props.value}>
+          {labelTitle}
+        </label>
 
         {mode === "edit" ? (
-          <Input {...(props as EditModeProps)} />
+          <div className="flex flex-row-reverse gap-2">
+            {props.Button}
+            <Input {...(props as EditModeProps)} id={props.value} />
+            {}
+          </div>
         ) : (
           <div
             className="px-4 py-2 text-gray-01 body-med-14 w-[272px] md:w-[312px] h-[41px]"
