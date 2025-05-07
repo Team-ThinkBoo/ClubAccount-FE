39243e3fd421fc1c45f4c5054b48bb486a4b878d
SeongIn Ko <jjjk0605@naver.com>
2025-05-07 14:04:48 +0900

    design: 일반 사용자 링크 스타일 수정

--- src/features/mypage/ViewContent.tsx
+++ src/features/mypage/ViewContent.tsx
@@ -24,7 +24,7 @@ const ViewContent = ({ mode }: ViewContentProps) => {
       <LabelInput labelTitle="가입일" mode={mode} value="2025년 2월 18일" />
       <LabelInput
         labelTitle="일반 사용자 링크"
-        className="overflow-hidden underline cursor-pointer whitespace-nowrap text-ellipsis"
+        className="overflow-hidden underline cursor-pointer body-med-14 whitespace-nowrap text-ellipsis"
         onClick={handleCopy}
         mode={mode}
         value={`${import.meta.env.VITE_BASE_URL}receipts/${link}`}
