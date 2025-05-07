3d467ba90ec5744a070317a5436804b4eaa5d983
SeongIn Ko <jjjk0605@naver.com>
2025-05-07 14:03:52 +0900

    feat: 일반 사용자링크 불러와서 클릭시 복사기능 구현

--- src/features/mypage/ViewContent.tsx
+++ src/features/mypage/ViewContent.tsx
@@ -1,16 +1,34 @@
+import { useAuthStore } from "@/store/useAuthStore";
 import LabelInput from "./LabelInput";
+import { toast } from "sonner";
 
 interface ViewContentProps {
   mode: "view";
 }
 
 const ViewContent = ({ mode }: ViewContentProps) => {
+  const { link } = useAuthStore();
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(`${import.meta.env.VITE_BASE_URL}receipts/${link}`);
+      toast.success("복사 완료!");
+    } catch {
+      toast.error("복사 실패");
+    }
+  };
   return (
     <>
       <LabelInput labelTitle="조직명" mode={mode} value="파산한 형제들" />
       <LabelInput labelTitle="이메일" mode={mode} value="test@test.com" />
       <LabelInput labelTitle="가입일" mode={mode} value="2025년 2월 18일" />
-      <LabelInput labelTitle="일반 사용자 링크" mode={mode} value="https://asdfasdfasdf.sadf" />
+      <LabelInput
+        labelTitle="일반 사용자 링크"
+        className="overflow-hidden underline cursor-pointer whitespace-nowrap text-ellipsis"
+        onClick={handleCopy}
+        mode={mode}
+        value={`${import.meta.env.VITE_BASE_URL}receipts/${link}`}
+      />
     </>
   );
 };
